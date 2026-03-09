dd3c2f0849dc86676e5736b1a800048e03922a98
Developmentexpert121 <developmentexpert121@gmail.com>
2026-03-09 11:11:03 +0530

    chore: remove temporary and debugging scripts

--- test_post.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.post(
-    "http://127.0.0.1:8001/admin-plans/",
-    data={"action": "update_plan", "plan_id": 1, "price": 1000}
-)
-print("status", response.status_code)
